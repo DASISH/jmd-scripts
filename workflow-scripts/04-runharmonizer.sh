9dbd11ccf9036661539759682d62243207fc063d
Kees Jan van de Looij <keeloo@m12404423.mpi.nl>
2014-11-06 09:43:21 +0100

    Temporarily disabled endpoints that are currently not harvestable. Moved DANS to CESSDA. Added paths in the harmonizer script.

--- workflow-scripts/04-runharmonizer.sh
+++ workflow-scripts/04-runharmonizer.sh
@@ -1,1 +1,1 @@
-python md-harmonizer.py -i work/02-mapped -c config.txt -o work/03-harmonized
+python md-harmonizer.py -i /home/work/work/02-mapped -c /home/work/jmd-scripts/workflow-scripts/config.txt -o /home/work/work/03-harmonized
