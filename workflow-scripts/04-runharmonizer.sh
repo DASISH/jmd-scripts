1cf01f59461424082552579dadedb7ed0746103d
Binyam <bini.belai@gmail.com>
2014-11-06 17:47:02 +0100

    Document 04-runharmonizer.sh

--- workflow-scripts/04-runharmonizer.sh
+++ workflow-scripts/04-runharmonizer.sh
@@ -1,1 +1,8 @@
-python md-harmonizer.py -i work/02-mapped -c config.txt -o work/03-harmonized
+input=/home/work/work/02-mapped-old # input directory with input of directories of json files
+output=/home/work/work/03-harmonized-test # output directory where directories of json files are saved
+config=config.txt #  normalization and replace rule file
+checksumTable=ctable.pkl # dictionary of file names and their hashes
+
+cd /home/work/jmd-scripts/workflow-scripts
+sudo python md-harmonizer.py -i $input -o $output -c $config -d $checksumTable
+
