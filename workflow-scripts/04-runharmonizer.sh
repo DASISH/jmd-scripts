ce48b4423b38b5244e8e91cb898ed15e49f71a6b
Binyam <bini.belai@gmail.com>
2014-11-06 19:13:01 +0100

    change location of checksum table

--- workflow-scripts/04-runharmonizer.sh
+++ workflow-scripts/04-runharmonizer.sh
@@ -2,7 +2,7 @@
 input=/home/work/work/02-mapped # input directory with input of directories of json files
 output=/home/work/work/03-harmonized # output directory where directories of json files are saved
 config=config.txt #  normalization and replace rule file
-checksumTable=ctable.pkl # dictionary of file names and their hashes
+checksumTable=/home/work/dict/ctable.pkl # dictionary of file names and their hashes
 
 cd /home/work/jmd-scripts/workflow-scripts
 sudo python md-harmonizer.py -i $input -o $output -c $config -d $checksumTable
