7cfe892ffa056e45167d2f10e8358fb2e01ff656
Binyam <bini.belai@gmail.com>
2014-11-14 15:43:13 +0100

    add local upload script [no http]

--- workflow-scripts/06-runuploader.sh
+++ workflow-scripts/06-runuploader.sh
@@ -6,5 +6,7 @@ apiKey=fb5734f2-7153-4efc-ac41-e532e38d9fca
 url=http://tlatest02.mpi.nl
 data2upload=/home/work/data2upload.jsonl
 
-ckanapi load datasets -I $data2upload -p $numberOfprocesses -r $url -a $apiKey
+#ckanapi load datasets -I $data2upload -p $numberOfprocesses -r $url -a $apiKey
 
+configFile=/etc/ckan/default/development.ini
+ckanapi load datasets -I $data2upload -p $numberOfprocesses -c $configFile
