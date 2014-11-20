5cd3fe6f34d6334b3f9fdd26518e1ef9f3c0ec37
Binyam <bini.belai@gmail.com>
2014-11-20 15:23:06 +0100

    Add description

--- workflow-scripts/06-runuploader.sh
+++ workflow-scripts/06-runuploader.sh
@@ -6,7 +6,10 @@ apiKey=fb5734f2-7153-4efc-ac41-e532e38d9fca
 url=http://tlatest02.mpi.nl
 data2upload=/home/work/data2upload.jsonl
 
+
+# uncomment for url-based uploading
 #ckanapi load datasets -I $data2upload -p $numberOfprocesses -r $url -a $apiKey
 
+# comment for url-based uploading
 configFile=/etc/ckan/default/development.ini
 ckanapi load datasets -I $data2upload -p $numberOfprocesses -c $configFile
