3ad799a6db5ef0e79955d1b8663a9a0f88018255
Kees Jan van de Looij <keeloo@m12404423.mpi.nl>
2014-12-05 10:28:23 +0100

    Added changes to make-jsonl.

--- scripts/workflow/make-jsonl.py
+++ scripts/workflow/make-jsonl.py
@@ -5,7 +5,7 @@ Binyam Gebrekidan Gebre
 HOW TO RUN
 $ python md-harmonizer.py -i /path/to/json -o /path/to/output.jsonline
 Input: a directory that has json files
-Output:jsonline file
+Output:jsonline file 
 """
 
 import os
@@ -14,7 +14,7 @@ import re
 import simplejson as json
 import io
 import codecs
-import pycountry
+import pycountry 
 import csv
 from urllib2 import urlopen
 import logging
@@ -23,7 +23,7 @@ import logging
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
-
+   
 def get_dataset(srcFile):
     """
     reads file from disk and returns json text
@@ -37,7 +37,7 @@ def get_user_input():
     """
     returns user input
     """
-    parser = argparse.ArgumentParser(description='Harmonizes json files')
+    parser = argparse.ArgumentParser(description='Makes jsonline file')
     parser.add_argument('-i','--input',help='input directory with json files')
     parser.add_argument('-o','--output', help='output file name')
 
@@ -49,24 +49,24 @@ def get_user_input():
     if not (root and output):
         print parser.print_help()
         exit(1)
-
+    
     return root, output
-
+    			
 def main():
-
+    
     # input from standard io
     srcDir, outputFile = get_user_input()
-
-
+    
+    
     # walks through json files and saves them as json lines
     batchSize = 100
     f = io.open(outputFile,'w',encoding='utf-8')
     jsonlines = ''
     num = 0
     for parent, subdirs, fnames in os.walk(srcDir):
-        srcFiles = filter(lambda x:x.endswith('.json'), fnames)
+        srcFiles = filter(lambda x:x.endswith('.json'), fnames) 
         if not srcFiles: continue
-
+        
         absparent = os.path.abspath(parent)
         for srcFile in srcFiles:
             path2file = os.path.join(absparent,srcFile)
@@ -78,7 +78,7 @@ def main():
             if num%batchSize == 0:
                 f.write(jsonlines)
                 jsonlines = ''
-
+    
     if jsonlines:
         f.write(jsonlines.strip())
 
